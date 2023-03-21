97fcb8bff3fe46bd4369ccde6cf054fda6916780
brayantunu <bstunubala@misena.edu.co>
2023-03-21 17:17:53 -0500

    router

--- router/controller-router/controller-router.js
+++ router/controller-router/controller-router.js
@@ -4,6 +4,8 @@ import { createpuntaje, deletepuntaje, getpuntaje, updatepuntaje } from "../../c
 import {getfuncionario,getfuncionarioid,createfuncionario,updatefuncionario,deletefuncionario} from   "../../controller/models-controller/funcionario-controller/funcionario-controller.js";
 import { CREATE_FUNCIONARIO_PRODUCTO, DELETE_FUNCIONARIO_PRODUCTO, GET_FUNCIONARIO_PRODUCTO, GET_FUNCIONARIO_PRODUCTO_ID, UPDATE_FUNCIONARIO_PRODUCTO } from "../../controller/models-controller/funcionario-producto-controller/funcionario-producto-controller.js";
 import { CREATE_SEMILLERO, DELETE_SEMILLERO, GET_SEMILLERO, GET_SEMILLERO_ID, UPDATE_SEMILLERO } from "../../controller/models-controller/semillero-controller/semillero-controller.js";
+import { CREATE_PRODUCTO_PROYECTO, DELETE_PRODUCTO_PROYECTO, GET_PRODUCTO_PROYECTO, GET_PRODUCTO_PROYECTO_ID, UPDATE_PRODUCTO_PROYECTO } from "../../controller/models-controller/producto_proyecto_controller/producto-proyecto-controller.js";
+import { CREATE_PROYECTO, DELETE_PROYECTO, GET_PROYECTO, GET_PROYECTO_ID, UPDATE_PROYECTO } from "../../controller/models-controller/proyecto-controller/proyecto_controller.js";
 const router = Router()
 
 // ROUTER THE PUNTAJES
@@ -41,5 +43,19 @@ router.post('/SEMILLEROS',CREATE_SEMILLERO)
 router.put('/SEMILLEROS/:id',UPDATE_SEMILLERO)
 router.delete('/SEMILLEROS/:id',DELETE_SEMILLERO)
 router.get('/SEMILLEROS/:id',GET_SEMILLERO_ID)
+
+// ROUTER PRODUCTO PROYECTO 
+router.get('/PRODUCTO_PROYECTO',GET_PRODUCTO_PROYECTO)
+router.post('/PROYECTO_PROYECTO',CREATE_PRODUCTO_PROYECTO)
+router.put('/PROYECTO_PROYECTO',UPDATE_PRODUCTO_PROYECTO)
+router.delete('/PROYECTO_PROYECTO',DELETE_PRODUCTO_PROYECTO)
+router.get('/PROYECTO_PROYECTO',GET_PRODUCTO_PROYECTO_ID)
+
+// ROUTER PROYECTO
+router.get('/PROYECTO',GET_PROYECTO)
+router.post('/PROYECTO',CREATE_PROYECTO)
+router.put('/PROYECTO',UPDATE_PROYECTO)
+router.delete('/PROYECTO',DELETE_PROYECTO)
+router.get('/PROYECTO',GET_PROYECTO_ID)
 
 export default router
