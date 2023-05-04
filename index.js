8936c39f1c0ac18b5dc26a1ae946241f0811759a
brayantunu <bstunubala@misena.edu.co>
2023-05-04 09:46:45 -0500

    agregando los modelos trl

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function main(){
     try {
         await sequelize.sync({force:false})
         app.listen(3000)
-        console.log("el proyecto y la base de datos estan conectados");
+        console.log("el proyecto y la base de datos estan conectados :)");
 
     } catch (error) {
         console.log('no hay conecxion con la base de datos',error);
--- models/calculadora-models/nivel-relactivo-models.js
+++ models/calculadora-models/nivel-relactivo-models.js
@@ -1,22 +1,20 @@
 import { DataTypes } from "sequelize"
 import { sequelize } from "../../db/db.js"
 
-export const n_relativo = sequelize.define('nivel_relativo', {
-    nivel_relativo_id: {
+export const nivel_relativo = sequelize.define('nivelRelativo', {
+    id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
 
     },
-    n_relativo_codigo_nivel: {
-        type: DataTypes.STRING
-    },
+    // n_relativo_codigo_nivel: {
+    //     type: DataTypes.STRING
+    // },
 
-    n_relativo_descripcion: {
-        type: DataTypes.STRING
-    },
+    // n_relativo_descripcion: {
+    //     type: DataTypes.STRING
+    // },
 
-},
-    {
-        timestamps: false
-    })
+}
+)
