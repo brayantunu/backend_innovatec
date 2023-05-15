ecb362e921bc36c7d9668b625e88695565ab2649
brayantunu <bstunubala@misena.edu.co>
2023-05-15 13:45:21 -0500

    se hiso un pull del servidor

--- router/controller-router/programa-router.js
+++ router/controller-router/programa-router.js
@@ -1,7 +1,11 @@
 import  {Router} from "express";
-import { programa } from "../../controller/programa-controller/programa-controller.js";
+import { create_programa, delete_programa, get_programa_id, programa, update_programa } from "../../controller/programa-controller/programa-controller.js";
 const router = Router()
 
 router.get("/programa",programa)
+router.post("/programa",create_programa)
+router.put("/programa",update_programa)
+router.delete("/programa",delete_programa)
+router.get("/programa/:programa_id",get_programa_id)
 
 export default router
