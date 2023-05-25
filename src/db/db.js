c5591743f34a4082263b82a446818937b2def928
brayantunu <bstunubala@misena.edu.co>
2023-05-25 18:01:27 -0500

    se hizo cambios

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,11 @@
 import Sequelize from "sequelize";
-import {DB_PORT,DB_USER,DB_PASSWORD,DB_DATABASE} from '../../config.js'
+import {DB_PORT,DB_USER,DB_PASSWORD,DB_DATABASE,DB_HOST} from '../../config.js'
 export const sequelize = new Sequelize(
     DB_DATABASE,//nombre de la base de datos
     DB_USER,//usuario
     DB_PASSWORD,//contraseña de la base de datos
     {
-        host: 'localhost',
+        host: DB_HOST,
         dialect: 'postgres',
         port: DB_PORT ,
         pool: {
