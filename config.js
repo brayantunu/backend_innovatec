35ec10a2db6eaf43d5e66478a1a37b597a7a36c3
brayantunu <bstunubala@misena.edu.co>
2023-05-25 17:45:59 -0500

    nuevos

--- config.js
+++ config.js
@@ -16,7 +16,10 @@ import {config} from 'dotenv'
 config()
 
 export const PORT = process.env.PORT || 3000
-export const DB_PORT = process.env.DB_PORT || 5432
+export const DB_PORT = process.env.DB_PORT
 export const DB_USER = process.env.DB_USER || 'postgres'
 export const DB_PASSWORD = process.env.DB_PASSWORD || '1007524913'
 export const DB_DATABASE = process.env.DB_DATABASE || 'final'
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+
+console.log(DB_DATABASE)
