8ad0ec59863c4acfe7e24fc5d1eb8ee1b199ad1b
brayan steven tunubala nieto <bstunbala@misena.edu.co>
2023-06-02 19:46:10 +0200

    prueba

--- src/db/db.js
+++ src/db/db.js
@@ -2,19 +2,17 @@
 import {Sequelize} from "sequelize";
 
 export const sequelize = new Sequelize(
-   process.env.DB_DATABASE || 'final1',//nombre de la base de datos
+   process.env.DB_DATABASE || 'final',//nombre de la base de datos
    process.env.DB_USER || 'postgres',//usuario
-    process.env.DB_PASSWORD || '0000',//contraseña de la base de datos
+    process.env.DB_PASSWORD || 'yuliana',//contraseña de la base de datos
     {
         host:process.env.DB_HOST || 'localhost',      
         dialect: 'postgres',
-        port:process.env.DB_PORT || 5432,
+        port:process.env.DB_PORT || 5433,
         pool: {
             max: 5,
             min: 0,
             require: 30000,
             idle: 10000
         },
-
-    })
-
+  })
